90de66d3076f8a96a31b0bc2ae7ccb996dfe7f0b
maneesha1718 <67682088+maneesha1718@users.noreply.github.com>
2024-06-01 17:16:46 +0530

    Approach2 App.js

--- App.js
+++ App.js
@@ -5,9 +5,8 @@ import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import { Fragment } from 'react';
 import Notification from './components/UI/Notification';
-import { cartItemsAction } from './store/cartItems';
-import { updateCartActions } from './store/updateCart';
-
+import { sendCartData } from './store/cartBackend';
+import { fetchCartData } from './store/cartBackend';
 
 let isInitial = true;
 
@@ -19,94 +18,17 @@ function App() {
 
   const dispatch = useDispatch();
 
+  useEffect(()=>{
+    dispatch(fetchCartData());
+  }, [dispatch]);
+
   useEffect(() => {
-    const sendCartData = async () => {
-
-      dispatch(
-        cartItemsAction.handleNotification({
-          status: 'pending',
-          title: 'Sending...',
-          message: 'Sending cart data!',
-        })
-      );
-
-      const response = await fetch(
-        'https://reducer-cart-default-rtdb.europe-west1.firebasedatabase.app/cart.json',
-        {
-          method: 'PUT',
-          body: JSON.stringify(cart),
-        }
-      );
-      if (!response.ok) {
-        throw new Error('Sending cart data failed.');
-      }
-
-      dispatch(
-        cartItemsAction.handleNotification({
-          status: 'success',
-          title: 'Success!',
-          message: 'Sent cart data successfully!',
-        })
-      );
-    };
-
     if (isInitial) {
       isInitial = false;
       return;
     }
-
-    sendCartData().catch((error) => {
-      dispatch(
-        cartItemsAction.handleNotification({
-          status: 'error',
-          title: 'Error!',
-          message: 'Sending cart data failed!',
-        })
-      );
-    });
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
-
-  useEffect(() => {
-    const fetchCartData = async () => {
-
-      dispatch(
-        cartItemsAction.handleNotification({
-          status: 'pending',
-          title: 'Sending...',
-          message: 'Sending cart data!',
-        })
-      );
-
-      const response = await fetch(
-        'https://reducer-cart-default-rtdb.europe-west1.firebasedatabase.app/cart.json',);
-
-      if (!response.ok) {
-        throw new Error('Fetching cart data failed.');
-      }
-
-      const data = await response.json();
-
-      dispatch(
-        updateCartActions.replaceCart(data)
-      );
-    };
-
-    if (isInitial) {
-      isInitial = false;
-      return;
-    }
-
-    fetchCartData().catch((error) => {
-      dispatch(
-        cartItemsAction.handleNotification({
-          status: 'error',
-          title: 'Error!',
-          message: 'Fetching cart data failed!',
-        })
-      );
-    });
-  }, [dispatch] )
-
 
   return (
     <Fragment>
